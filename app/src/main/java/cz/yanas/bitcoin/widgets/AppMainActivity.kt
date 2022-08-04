b8f0ca7c855b9c0d90e18e780d3f862a1787448a
yanas <yanas@yanas.cz>
2022-08-04 08:27:56 +0200

    Add link to privacy policy to main view

--- app/src/main/java/cz/yanas/bitcoin/widgets/AppMainActivity.kt
+++ app/src/main/java/cz/yanas/bitcoin/widgets/AppMainActivity.kt
@@ -13,7 +13,7 @@ class AppMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_main)
 
-        for (viewId in listOf(R.id.source_code, R.id.issue_tracking, R.id.lightning_address)) {
+        for (viewId in listOf(R.id.source_code, R.id.issue_tracking, R.id.privacy_policy, R.id.lightning_address)) {
             findViewById<TextView>(viewId).movementMethod = LinkMovementMethod.getInstance()
         }
 
